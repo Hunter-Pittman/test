feb4d3b619e0ce7228e8e63e9d6024ec7f3174e2
Hunter-Pittman <hunterpittman@protonmail.com>
2023-02-10 12:58:37 -0800

    Added variables Added printf's

--- main.go
+++ main.go
@@ -4,4 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	firstName := "Hunter"
+	lastName := "Pittman"
+
+	var d uint8
+	d = 255
+
+	fmt.Printf("My name is %v %v \n", firstName, lastName)
+	fmt.Printf("My number is %v", d)
+	goodbye()
+
+}
+
+func goodbye() {
+	fmt.Println("Goodbye!")
 }
